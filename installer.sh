c7549941283daa2bac35d9223e17079201947a75
oytunmw <oytun@motaword.com>
2021-05-22 12:06:31 +0300

    installer.sh updated for v1.3.2

--- installer.sh
+++ installer.sh
@@ -15,7 +15,7 @@ function install {
 	USER="motaword"
 	PROG="motaword"
 	MOVE="false"
-	RELEASE="v1.3.1"
+	RELEASE="v1.3.2"
 	INSECURE="false"
 	OUT_DIR="$(pwd)"
 	GH="https://github.com"
@@ -61,24 +61,24 @@ function install {
 	URL=""
 	FTYPE=""
 	case "${OS}_${ARCH}" in
-	"darwin_arm")
-		URL="https://github.com/MotaWord/cli/releases/download/v1.3.1/motaword_1.3.1_Darwin_arm64.tar.gz"
-		FTYPE=".tar.gz"
-		;;
-	"darwin_amd64")
-		URL="https://github.com/MotaWord/cli/releases/download/v1.3.1/motaword_1.3.1_Darwin_x86_64.tar.gz"
+	"linux_arm")
+		URL="https://github.com/MotaWord/cli/releases/download/v1.3.2/motaword_1.3.2_Linux_arm64.tar.gz"
 		FTYPE=".tar.gz"
 		;;
 	"linux_386")
-		URL="https://github.com/MotaWord/cli/releases/download/v1.3.1/motaword_1.3.1_Linux_i386.tar.gz"
+		URL="https://github.com/MotaWord/cli/releases/download/v1.3.2/motaword_1.3.2_Linux_i386.tar.gz"
+		FTYPE=".tar.gz"
+		;;
+	"darwin_amd64")
+		URL="https://github.com/MotaWord/cli/releases/download/v1.3.2/motaword_1.3.2_Darwin_x86_64.tar.gz"
 		FTYPE=".tar.gz"
 		;;
 	"linux_amd64")
-		URL="https://github.com/MotaWord/cli/releases/download/v1.3.1/motaword_1.3.1_Linux_x86_64.tar.gz"
+		URL="https://github.com/MotaWord/cli/releases/download/v1.3.2/motaword_1.3.2_Linux_x86_64.tar.gz"
 		FTYPE=".tar.gz"
 		;;
-	"linux_arm")
-		URL="https://github.com/MotaWord/cli/releases/download/v1.3.1/motaword_1.3.1_Linux_arm64.tar.gz"
+	"darwin_arm")
+		URL="https://github.com/MotaWord/cli/releases/download/v1.3.2/motaword_1.3.2_Darwin_arm64.tar.gz"
 		FTYPE=".tar.gz"
 		;;
 	*) fail "No asset for platform ${OS}-${ARCH}";;
